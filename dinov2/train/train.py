70dc98085cbd5f7bf1b1c2c963ccf833b7dffe73
raunak-agarwal <24976972+raunak-agarwal@users.noreply.github.com>
2024-08-15 00:23:55 +0200

    reduce logging freq

--- dinov2/train/train.py
+++ dinov2/train/train.py
@@ -221,7 +221,7 @@ def do_train(cfg, model, resume=False):
 
     for data in metric_logger.log_every(
         data_loader,
-        5,
+        10,
         header,
         max_iter,
         start_iter,
